254f9057ea1f621d2d1199c4e60d23b18dd2cd04
ivanovich18 <ivan.suralta@ctu.edu.ph>
2024-08-02 08:44:32 +0800

    Updated firebase.js to use env

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: process.env.GOOGLE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
